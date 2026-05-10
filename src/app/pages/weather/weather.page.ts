a45e79eeef2acd6e08362d3067164589965a745b
leoVPires <leonardovasconcelos73@gmail.com>
2026-05-10 15:05:40 -0300

    create side-bar component and change light theme to improve contrast

--- src/app/pages/weather/weather.page.ts
+++ src/app/pages/weather/weather.page.ts
@@ -1,20 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import {
+  IonContent,
+  IonHeader,
+  IonTitle,
+  IonToolbar,
+  IonMenuButton,
+  IonButtons,
+} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-weather',
   templateUrl: './weather.page.html',
   styleUrls: ['./weather.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [
+    IonContent,
+    IonHeader,
+    IonTitle,
+    IonToolbar,
+    IonMenuButton,
+    IonButtons,
+    CommonModule,
+    FormsModule,
+  ],
 })
 export class WeatherPage implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
